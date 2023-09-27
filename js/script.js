793fdc83a1e4962a8b3ff66bd06cb7450b29b00a
Co Bai Ly <col@uci.edu>
2023-09-27 02:27:37 -0700

    completed assignment 1

--- js/script.js
+++ js/script.js
@@ -1,36 +1,144 @@
 // STEP 1
+someMonth
+theMonth()
+currentMonth
+summerMonth
+myLibraryFunction
+
 
 // STEP 2
+9  // Numberic Literal Expression
+"Hello World"  // String Literal Expression
+true  // Boolean Literal Expression
+null  // Null Literally Expression
+
 
 // STEP 3
+let addition = 9 + 10
+let subtraction = 11 - 2
+
 
 // STEP 4
+let strFirstName
+let strLastName
+let strCity
+let strState
+let strZipCode
+let intYourAge
+let strReferralSource
+let blnMayWeContactYou
+
 
 // STEP 5
+let strCity
+let intYourAge
+let blnMayWeContactYou
+strCity = "San Diego"
+intYourAge = 26
+blnMayWeContactYou = true
+
+let strCity = "San Diego"
+let intYourAge = 26
+let blnMayWeContactYou = true
+
+let strCity = "San Diego", intYourAge = 26, blnMayWeContactYou = true
+
 
 // STEP 6
+let animal = "Panda"
+let num = 5
+console.log(animal + num)
+
 
 // STEP 7
+let firstBln = true
+let firstStr = "This is false"
+console.log(firstBln + firstStr)
+
+let secondInt = 10
+let secondBln = false
+console.log(secondInt + secondBln)
+
 
 // STEP 8
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString)
+
 
 // STEP 9
+null_var = null
+console.log(null_var)
+
+undef_var = undefined
+console.log(undef_var)
+
 
 // STEP 10
+console.log(typeof('Hello World'))
+console.log(typeof(1))
+console.log(typeof(true))
+console.log(typeof(console))
+console.log(typeof(undefined))
+
 
 // STEP 11
+alert('Hello ' + 'Co Ly' + ', welcome to the JavaScript class!')
+
 
 // STEP 12
+name = 'Co Ly'
+alert('Hello ' + name + ', welcome to the JavaScript class!')
+
 
 // STEP 13
+name = 'Co Ly'
+course = 'JavaScript'
+alert('Hello ' + name + ', welcome to the ' + course + ' class!')
+
 
 // STEP 14
+alert('Hello Co Ly. \n Welcome to the JavaScript class!')
+
 
 // STEP 15
+name = prompt('What is your name?')
+alert('Hello ' + name + '.\nWelcome to the JavaScript class!')
+
+
 // STEP 16
+course = prompt('What class are you taking?')
+alert('Hello User.\nWelcome to the ' + course + ' class!')
+
+
 // STEP 17
+let x = 10
+let y = 20
+console.log(x + y)
+
+
 // STEP 18
+let x = 20
+console.log(x + 20)
+
+
 // STEP 19
+let x = 20
+console.log(x * 5)
+
+
 // STEP 20
+let x = 20 % 3
+console.log(x / 1)
+
+
 // STEP 21
+let x = 10
+let y = 20
+console.log(x < y)
+
+
 // STEP 22
+let x = 5
+let y = 10
+let z = 3
+console.log(!(y < (x * z)))
